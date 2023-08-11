ea19bc91b5869d0d02b923475664d87fbedf67a5
Chinwe-MC <chichuks02@gmail.com>
2023-08-11 15:00:37 -0700

    Added a file that prints base 10 numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *main - Print digitt 1 - 0 using putchar
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
