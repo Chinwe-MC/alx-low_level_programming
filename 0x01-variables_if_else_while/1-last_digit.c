7b73a81b384cc8a0dd698b1a7b0c2f68e59a8b95
Chinwe-MC <chichuks02@gmail.com>
2023-08-11 09:55:41 -0700

    Added a file that prints last number generated

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,6 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-/* main - Entry point */
-/* Return: Always 0 (Success) */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+	int i;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	i = n;
+	if ( n > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, i);
+	}
+	else if (n == 0)
+	{
+		printf("Last digit of %d is %d and is zero\n", n, i);
+	}
+	else if (n < 6 && n != 0)
+	{
+		printf("Last digit of %d is %d and not 0\n", n, i);
+	}	
+	return (0);
+}
  
