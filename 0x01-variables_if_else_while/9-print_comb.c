cf178d5a4caad8379c2db5e1078b2db27c38a7f4
Chinwe-MC <chichuks02@gmail.com>
2023-08-11 16:12:56 -0700

    added a file that prints numbers follwed by comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - print all possible combination of single digit, comma seperated
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+
+}
