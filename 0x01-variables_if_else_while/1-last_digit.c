55f29a01279a2c46cf441d546a89df51f5c73238
Chinwe-MC <chichuks02@gmail.com>
2023-08-11 10:45:03 -0700

    Added a file that prints last number generated

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,13 +23,11 @@ int main(void)
 	}
 	else if (i == 0)
 	{
-		printf("Last digit of %d is %d and is zero\n", n, i);
+		printf("Last digit of %d is %d and is %d\n", n, i, i);
 	}
 	else if (i < 6 && i != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, i);
-	}	
+	}
 	return (0);
-	
 }
- 
