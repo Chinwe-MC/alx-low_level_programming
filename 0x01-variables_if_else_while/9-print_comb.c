5a7b2107233ab9c8dcf2ed0253395ef51bcaff63
Chinwe-MC <chichuks02@gmail.com>
2023-08-11 17:19:00 -0700

    Added a file the prints digis with comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,17 @@ int main(void)
 {
 	int i;
 	
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <=9; i++)
 	{
 		putchar('0' + i);
+		if (i == 9)
+		{
+			continue;
+		}
 		putchar(',');
 		putchar(' ');
 	}
-		
-	
+	putchar(' ');
 	return (0);
 
 }
