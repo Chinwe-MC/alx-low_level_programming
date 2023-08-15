55e5b38e4cea072ec527a317fa710edeef689fb7
Chinwe-MC <chichuks02@gmail.com>
2023-08-15 12:03:54 -0700

    A file that prints lettes ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+
+	char c = 97;
+	int i;
+
+	for (i = 48; i <=57; i++)
+	{
+		for (c = 97; c <= 122; c++)
+		{
+			_putchar(c);
+	
+		}
+		_putchar('\n');
+	}
+}
+	
