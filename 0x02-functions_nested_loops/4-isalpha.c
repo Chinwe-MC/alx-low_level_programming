839916a99185a947a087f095ad47356850917a59
Chinwe-MC <chichuks02@gmail.com>
2023-08-15 12:32:14 -0700

    A program that checks alphabeths

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * 4-isalpha.c - A function that checks for aplhabeths
+ * _isalpha - A function that checks for aplhabeths
  *
  * Return: 1 if alphabeth else Return 0
 */
@@ -12,10 +12,10 @@ int _isalpha(int c)
 
 	if (isalpha(c))
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
