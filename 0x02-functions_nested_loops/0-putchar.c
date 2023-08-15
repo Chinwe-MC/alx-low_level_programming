edca24c4c0ea772f2d6f6d302e4ee4d4b0084ac6
Chinwe-MC <chichuks02@gmail.com>
2023-08-15 08:10:47 -0700

    Added a file that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printing a word and a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	printf("_putchar");
+	print("\n");
+	
+	return (0);
+}
+
