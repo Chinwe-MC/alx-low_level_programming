4350755adde505a203cc1587bc03795fd94f2e1b
Chinwe-MC <chichuks02@gmail.com>
2023-08-15 13:38:40 -0700

    A program thr prints the absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * _abs - Checks the absolute number
+ *
+ * @int: function parameter int
+ *
+ * Return: Always 0
+*/
+
+int _abs(int a)
+{
+	_putchar(abs('0' + a));
+	return (0);
+
+}
