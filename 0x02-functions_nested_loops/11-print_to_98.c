795c5199c8a2466011ded14fc79b5ab218e4f183
Chinwe-MC <chichuks02@gmail.com>
2023-08-15 18:16:47 -0700

    Print numbers in orderly manner

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+
+
+void print_to_98(int n)
+{
+	
+	int i;
+	
+	for (i = n;  i <= 98; i++)
+	{
+		if ( i <= 98)
+		{
+			printf("%d ", i);
+			n++;
+		}
+	
+
+		else if ( i > 98)
+		{
+			printf("%d ", i);
+			n++;
+		}
+	
+	}
+	printf("\n);
+	
+}
+
