f25de804b2abf6ebfb7d8f7537484cee32dc5da1
Chinwe-MC <chichuks02@gmail.com>
2023-08-15 17:36:44 -0700

    A program that adds two numbers;

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * add - function that adds two ints
+ *
+ * @a: function parameter
+ * @b:  function parameter
+ * Return: return result
+*/
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
