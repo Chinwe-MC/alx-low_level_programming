362603a3166560ac905de7f4a311716562a97d28
Chinwe-MC <chichuks02@gmail.com>
2023-08-15 20:52:15 -0700

    printing fibonnaci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 for success
+ */
+
 int main(void)
 {
-	int fib = 50;
-	
-	for (
+	int f[50];
+	f[0] = 1;
+	f[1] = 2;
+	int i;
+
+	for (i = 2; i < 50; i++)
+	{
+		f[i] = f[i - 1] + f[i - 2];
+	}
+	for (i = 0; i < 50; i++)
+	{
+		printf("%d", f[i]);
+		if (i < 49)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
