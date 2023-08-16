4cff2c05f56cb291922c479f8203c1da405675e4
Chinwe-MC <chichuks02@gmail.com>
2023-08-16 15:16:49 -0700

    Detecting endless loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,20 +7,18 @@
 
 int main(void)
 {
-	int i;
+ 	int i;
 
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
- /**     
- * while (i < 10) while loop causes infinte loop here, need to increment 1
- *
- * {
- * putchar(i);
- * }
- */
- 	printf("Infinite loop avoided! \\o/\n");
+	/**
+        *while (i < 10)
+        *{
+        *        putchar(i);
+        *}
+	*/
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
