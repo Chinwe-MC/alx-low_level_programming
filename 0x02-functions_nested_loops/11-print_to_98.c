a2cffcd61bc26b598e3ce8532d8630597618c351
Chinwe-MC <chichuks02@gmail.com>
2023-08-15 18:51:56 -0700

    Print numbers orderly

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,25 +10,30 @@
 void print_to_98(int n)
 {
 	
-	int i;
-	
-	for (i = n;  i <= 98; i++)
+	if (n <=98)
 	{
-		if ( i <= 98)
+		for (n = n; n <= 98; n++)
 		{
-			printf("%d ", i);
-			n++;
+		
+			printf("%d, ", n);
+			continue;
 		}
+	
+	printf("\n");
+	}
+
+	else if (n > 98)
+	{
+		for (n = n; n > 98; n--)
+		{
+		
+			printf("%d, ", n);
+		}
+		printf("98\n");
+		
+	}
 	
 
-		else if ( i > 98)
-		{
-			printf("%d ", i);
-			n++;
-		}
-	
-	}
-	printf("\n");
 	
 }
 
