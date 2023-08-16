50a4bcc17d82085a734e95baba92bc47f63ebca5
Chinwe-MC <chichuks02@gmail.com>
2023-08-15 19:07:53 -0700

    print numbers;

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,28 +12,15 @@ void print_to_98(int n)
 	
 	if (n <=98)
 	{
-		for (n = n; n <= 98; n++)
-		{
-		
-			printf("%d, ", n);
-			continue;
-		}
-	
-	printf("\n");
+		for (n = n; n <= 97; n++)
+			printf("%d, " , n);
 	}
 
-	else if (n > 98)
-	{
+	else
+	
 		for (n = n; n > 98; n--)
-		{
 		
 			printf("%d, ", n);
-		}
-		printf("98\n");
-		
-	}
-	
-
-	
+	printf("98\n");
 }
 
