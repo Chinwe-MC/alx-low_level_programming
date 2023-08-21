4ba0d035c73fda180482b3f40c7deb1b2784c982
Chinwe-MC <chichuks02@gmail.com>
2023-08-21 16:54:07 -0700

    convert int to strint

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ *_atoi: Function name
+ *@s: function parameter
+ *Retun _atoi: int
+ */
+
+int _atoi(char *s)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int len = 0;
+	int num = 0;
+	int i = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && c == 0)
+	{
+		if (s[i] == '-')
+			++a;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = s[i] - '0';
+			if (a % 2)
+				num = -num;
+			b = b * 10 + num;
+			c = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			c = 0;
+		}
+		i++;
+	}
+		if (c == 0)
+			return (0);
+		return (b);
+}
