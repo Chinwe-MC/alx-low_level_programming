a185d3fa242d8a6fe7f56c7639f1b91064bc8346
Chinwe-MC <chichuks02@gmail.com>
2023-08-24 14:15:56 -0700

    Compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,30 +5,26 @@
  *@s1: function parameter
  *@s2: function parameter
  *Return: int
- */
+*/
 
 int _strcmp(char *s1, char *s2)
 {
-	int s1counter = 0;
-	int s2counter = 0;
-
-	while (*s1 != '\0')
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		s1counter++;
-		s1++;
+	if (s1 != s2)
+	{
+		return(*s1 - *s2);
 	}
-	while (*s2 != '\0')
-	{
-		s2counter++;
+		s1++;
 		s2++;
 	}
-	if (s1counter < s2counter)
+	if (*s1 == '\0' && *s2 != '\0')
 	{
-		return (-15);
+		return -(*s1);
 	}
-	else if (s1counter > s2counter)
-	{
-		return (15);
+	else if (*s1 != '\0' && *s2 == '\0')
+	{	
+		return (*s2);
 	}
 	return (0);
 }
