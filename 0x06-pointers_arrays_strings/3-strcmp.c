170f3966e6273e548bf6d685044435f938587d18
Chinwe-MC <chichuks02@gmail.com>
2023-08-24 14:30:45 -0700

    Compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,18 +13,18 @@ int _strcmp(char *s1, char *s2)
 	{
 	if (s1 != s2)
 	{
-		return(*s1 - *s2);
+		return (*s1 - *s2);
 	}
 		s1++;
 		s2++;
 	}
 	if (*s1 == '\0' && *s2 != '\0')
 	{
-		return -(*s1);
+		return (-*s2);
 	}
 	else if (*s1 != '\0' && *s2 == '\0')
-	{	
-		return (*s2);
+	{
+		return (*s1);
 	}
 	return (0);
 }
