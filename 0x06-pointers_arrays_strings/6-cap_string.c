50ae8feba615d13dc702fd222e2ee7ffca0d75a4
Chinwe-MC <chichuks02@gmail.com>
2023-08-24 17:38:54 -0700

    capitalize each word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <ctype.h>
 
 
@@ -11,33 +10,27 @@
 
 char *cap_string(char *str)
 {
-	bool word = true;
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i])
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-		str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-		str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-		str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
-		{
-		word = true;
-	}
-	else
-	{
-		if (word)
-		{
-		str[i] = toupper(str[i]);
-		word = false;
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+		
+		i++;
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+		str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+		str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+		str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}' ||
+		i == 0)
+			str[i] -=32;
+		i++;
 		}
-		else
-		{
-			str[i] = tolower(str[i]);
-		}
-	}
-	}
-	return (str);
+	
+		return (str);
+		
 }
+
+		
 
 
 
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+char *leet(char *)
+{
+	int i = 0;
+
+	for ( i = 0; i != '\0'; i++)
+	{
+		if (str[i] == 'a' || str[i] == 'A' || str[i] == 'e' || str[i] == 'E' ||
+				str[i] == 'o' || str[i] == 'O'
