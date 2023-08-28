e6da70daa87914a6b3160943d834a055c8903983
Chinwe-MC <chichuks02@gmail.com>
2023-08-28 13:42:36 -0700

    String search

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ *_strchr - Function name
+ *@s: function parameter
+ *@c: function parameter
+ *Return: NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+
+
+}
