45194c87c052b93bb5cccc3680d4e2823bd9faa9
Chinwe-MC <chichuks02@gmail.com>
2023-08-30 15:44:59 -0700

    Get the length of an int

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strlen_recursion - gets length of an int
+ *@s: function parameter
+ *Return: integer
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
+
