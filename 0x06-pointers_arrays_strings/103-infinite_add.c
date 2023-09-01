52252a614473a35d06e9b2aee5df80be604f0345
Chinwe-MC <chichuks02@gmail.com>
2023-09-01 16:13:34 -0700

    Print number of arguments

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,12 +1,45 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
-/**
- *
- *
- *
- *
- */
+char *infinite_add(char *n1, char *n2, char *r, int size_r) {
+    int len1 = strlen(n1);
+    int len2 = strlen(n2);
+    int i;
+    
+    int carry = 0;
+    int maxLen = (len1 > len2) ? len1 : len2;
+    
+    if (maxLen  > size_r) {
+        return 0; 
+    }
+    
+    r[maxLen + 1] = '\0';
+    
+    for (i = 0; i < maxLen; i++) {
+        int digit1, digit2, sum;
 
- char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	r = n1 + n2;
+	if (i < len1) {
+    		digit1 = n1[len1 - 1 - i] - '0';
+	} else {
+    		digit1 = 0;
+	}
+	if (i < len2) {
+    		digit2 = n2[len2 - 1 - i] - '0';
+	} else {
+    		digit2 = 0;
+	}
+        
+        	sum = digit1 + digit2 + carry;
+        	carry = sum / 10;
+        
+        	r[maxLen - i] = (sum % 10) + '0';
+    }
+    
+    if (carry) {
+        r[0] = carry + '0';
+        return r;
+    } else {
+       return r + 1; 
+    }
+}
