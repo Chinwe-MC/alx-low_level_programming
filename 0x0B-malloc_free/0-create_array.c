35feb517b4ac7934809d9908149632d76593ef1a
Chinwe-MC <chichuks02@gmail.com>
2023-09-05 05:16:44 -0700

    allocate space for an using malloc and assign character to arr

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ *create_array - A function that creates and initializes an array with chars
+ *@size: size of the array
+ *@c: character initialized to the array
+ *Return: a pointer to the array characters
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *arr;
+
+	if (size == 0)
+		return (NULL);
+	arr = (char *)malloc(size * sizeof(char));
+	while (i < size)
+	{
+		arr[i] = c;
+		i++;
+	}
+	return (arr);
+}
+
+
