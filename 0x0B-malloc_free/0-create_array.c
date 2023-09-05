ab9acdad9dc51dd4e73f79f1b61e24215a09c253
Chinwe-MC <chichuks02@gmail.com>
2023-09-05 05:24:54 -0700

    allocate space for an using malloc and assign character to arr

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,9 +15,9 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
+	arr = (char *)malloc(size * sizeof(char));
 	if (arr == NULL)
 		return NULL;
-	arr = (char *)malloc(size * sizeof(char));
 	while (i < size)
 	{
 		arr[i] = c;
