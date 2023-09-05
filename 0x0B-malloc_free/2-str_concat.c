3852bf87d571ec0c573ffabcc215c6063d06b702
Chinwe-MC <chichuks02@gmail.com>
2023-09-05 15:30:20 -0700

    Concatenate two strings and make memory for new string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ *str_concat - function to concatenate two strings
+ *@s1: destination argument
+ *@s2: source argument
+ *Return: concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	int length1 = 0;
+	int length2 = 0;
+	char *temp;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	/*get length of s1*/
+	while (s1[length1] != '\0')
+	{
+		length1++;
+	}
+	/*Get lengtt of s2*/
+	while (s2[length2] != '\0')
+	{
+		length2++;
+	}
+	/*allocate memory for new string*/
+	temp = (char *)malloc(length1 + length2 + 1 * sizeof(char));
+	for (i = 0; i < length1; i++)
+	{
+		temp[i] = s1[i];
+	}
+	for (i = 0; i < length2; i++)
+	{
+		temp[length1 + i] = s2[i];
+	}
+	temp[length1 + length2] = '\0';
+	if (s1 == NULL)
+	{
+		return (NULL);
+	}
+	return (temp);
+
+}
