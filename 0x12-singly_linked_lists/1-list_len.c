9638e7ece515d0fa13b4cf23b0ffd0d1cc47895a
Chinwe-MC <chichuks02@gmail.com>
2023-09-22 16:27:38 -0700

    print number of elements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,9 +2,9 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ *list_len - function name
+ *@h: parameter
+ *Return: Always 0
  */
 
 
@@ -12,7 +12,7 @@ size_t list_len(const list_t *h)
 {
 	size_t i = 0;
 
-	while(h)
+	while (h)
 	{
 		i++;
 		h = h->next;
