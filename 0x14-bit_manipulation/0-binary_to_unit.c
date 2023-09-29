430635160f92eaf85ab991f67adabb3964be7d36
Chinwe-MC <chichuks02@gmail.com>
2023-09-29 00:04:37 -0700

    bit manipulatioan

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int output = 0;
+	int i = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while (b[i] != '\0')
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			output = output * 2 + (b[i] - '0');
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (output);
+}
+
+
