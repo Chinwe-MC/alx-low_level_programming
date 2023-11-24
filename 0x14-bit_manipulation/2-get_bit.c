8068e6c3d0d86dff73c878af10c1e1affadad3d0
Chinwe-MC <chichuks02@gmail.com>
2023-11-24 11:17:56 -0800

    return value of a bit at given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,13 +12,13 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int divider, check;
 
-	if (index >(sizeof(unsigned long int) * 8 - 1))
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 	{
 		return (-1);
 	}
 	divider = 1 << index;
 	check = n & divider;
-	if ( check == divider)
+	if (check == divider)
 	{
 		return (1);
 	}
