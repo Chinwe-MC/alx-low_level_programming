e54bd00291a91a1002e3e520a987ffadb9b59d65
Chinwe-MC <chichuks02@gmail.com>
2023-11-27 21:24:22 -0800

    function to create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+
+/**
+ *create_file - function name
+ *@filename: parameter
+ *@text_content: parameter
+ *Return: Success 0
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytesWritten;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		close(fd);
+		if (bytes_written == -1)
+		{
+			return (1);
+		}
+	}
+	else
+	{
+		close(fd);
+	}
+
+	return (1);
+}
+
