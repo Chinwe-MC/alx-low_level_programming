f6ab0491690d32fbb98cead72a1a53d0d145568e
Chinwe-MC <chichuks02@gmail.com>
2023-11-27 20:33:02 -0800

    function that reads a text file and prints

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,6 +3,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+/**
+ *read_textfile - function name
+ *@filename: parameter
+ *@letters: parameter
+ *Return: Always 0
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
@@ -10,41 +16,38 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytesRead;
 	ssize_t bytesWritten;
 
-    if (filename == NULL)
-    {
-        return (0);
-    }
+	if (filename == NULL)
+	{
+	return (0);
+	}
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        return (0);
-    }
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+	return (0);
+	}
 
-    buffer = (char *)malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
-
-    bytesRead = read(fd, buffer, letters);
-    if (bytesRead == -1)
-    {
-        close(fd);
-        free(buffer);
-        return (0);
-    }
-
-    bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
-    if (bytesWritten == -1 || bytesWritten != bytesRead)
-    {
-        close(fd);
-        free(buffer);
-        return (0);
-    }
-
-    close(fd);
-    free(buffer);
-    return (bytesWritten);
+	buffer = (char *)malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+	close(fd);
+	return (0);
+	}
+	bytesRead = read(fd, buffer, letters);
+	if (bytesRead == -1)
+	{
+	close(fd);
+	free(buffer);
+	return (0);
+	}
+	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+	if (bytesWritten == -1 || bytesWritten != bytesRead)
+	{
+	close(fd);
+	free(buffer);
+	return (0);
+	}
+	close(fd);
+	free(buffer);
+	return (bytesWritten);
 }
